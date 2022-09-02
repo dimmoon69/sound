4b73f889c48d95bb9ba93f7895724d45011910d4
Дима <pldimitry@yandex.ru>
2022-09-02 16:23:35 +0300

    Поменял нумерацию значений

--- calculator.py
+++ calculator.py
@@ -27,13 +27,13 @@ class Calc:
             self.again()
 
     def get_value(self):
-        for num in range(10, 200, 10):
-            for number, letter in enumerate(self.letters):
-                start = round(self.start * COEFFICIENT, 8)
-                self.calc.append((f"{num}:{letter}", start))
+        for num in range(1, 200, 1):
+            # for number, letter in enumerate(self.letters):
+            start = round(self.start * COEFFICIENT, 8)
+            self.calc.append((f"{self.to_base_12(num)}", start))
 
     def start_value(self):
-        if self.value_1 == "10:c":
+        if self.value_1 == "1":
             self.start = self.value_2
             return True
         else:
@@ -55,15 +55,15 @@ class Calc:
             b += alpha[num % base]
         return b[::-1]
 
-    def save_dict(self, num, data_num, letter, interval):
-        self.data[num][data_num] = f"{num}:{letter}"
+    def save_dict(self, num, data_num, interval):
+        self.data[num][data_num] = self.to_base_12(self.num_interval)
         self.data[f"Значение {num}"][data_num] = self.start
         self.data[f"Интервaл {num}"][data_num] = ""
         self.data[f"Частота Гц {num}"][data_num] = ""
         data_num += 1
         self.data[num][data_num] = ""
         self.data[f"Значение {num}"][data_num] = ""
-        self.data[f"Интервaл {num}"][data_num] = self.to_base_12(self.num_interval)
+        self.data[f"Интервaл {num}"][data_num] = self.num_interval
         self.data[f"Частота Гц {num}"][data_num] = interval
         data_num += 1
         return data_num
@@ -76,9 +76,9 @@ class Calc:
         self.again()
 
     def calculator(self):
-        self.value_1 = input("Введите переменную: ") or "20:n"
+        self.value_1 = input("Введите переменную: ") or "1"
         try:
-            self.value_2 = float(input("Введите значение: ") or 4.719)
+            self.value_2 = float(input("Введите значение: ") or 1)
         except:
             print("\nПроверьте введенное значение!")
             self.again()
@@ -87,29 +87,22 @@ class Calc:
         check = self.start_value()
 
         if check:
-            for num in range(10, 200, 10):
+            for num in range(1, 25, 1):
                 data_num = 0
                 self.data[num] = {}
                 self.data[f"Значение {num}"] = {}
                 self.data[f"Интервaл {num}"] = {}
                 self.data[f"Частота Гц {num}"] = {}
-                for number, letter in enumerate(self.letters):
+                for number in range(1, 13):
                     self.interval = round(self.start * COEFFICIENT - self.start, 8)
                     self.num_interval += 1
-                    if number == 0:
-                        data_num = self.save_dict(num, data_num, letter, self.interval)
-                    else:
-                        data_num = self.save_dict(num, data_num, letter, self.interval)
-                    # print(f"{num}:{letter} = {self.start} "
-                    #       f"\n\t\tИнтервал: {self.to_base_12(self.num_interval)} = {self.interval}")
+                    data_num = self.save_dict(num, data_num, self.interval)
                     self.start = round(self.start * COEFFICIENT, 8)
                     self.total_interval.append(self.interval)
                 self.data[f"Частота Гц {num}"][data_num] = ""
                 data_num += 1
                 self.data[f"Частота Гц {num}"][data_num] = '{:.2f}'.format(sum(self.total_interval))
-                # print("Сумма:" + "-" * 20, round(sum(self.total_interval)))
                 self.total_interval.clear()
-            # print(self.data)
             self.save_file()
         else:
             print("Проверьте введенную переменную!")
